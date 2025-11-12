92c0bb8cf26002388958a106a75143d5516c3788
Miracle <miraclesteven05@gmail.com>
2025-11-12 17:01:16 +0700

    feat: Class creation & Docker connection configured

---
+++ src/main/java/com/miracleous/SoftwareEngineerService.java
@@ -0,0 +1,24 @@
+package com.miracleous;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SoftwareEngineerService {
+
+    private final SoftwareEngineerRepository softwareEngineerRepository;
+
+    public SoftwareEngineerService(SoftwareEngineerRepository softwareEngineerRepository) {
+        this.softwareEngineerRepository = softwareEngineerRepository;
+    }
+
+    // Returning "SoftwareEngineer" which is an entity
+    // Usually, you don't want to do this, but map it into a DTO (because there might be some data that doesn't want to be returned to the client, i.e. password)
+    public List<SoftwareEngineer> getAllSoftwareEngineers() {
+        return softwareEngineerRepository.findAll();
+
+        // Best practices:
+//        return softwareEngineerRepository.findAll().stream().map([A class that you can return to the client]);
+    }
+}
